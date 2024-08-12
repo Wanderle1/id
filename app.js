d0988a09bdf499d880689d8d90d79ed84a819051
Wanderle1 <wanderlei.mora3s@gmail.com>
2024-08-12 17:18:37 -0300

    w

--- app.js
+++ app.js
@@ -19,11 +19,20 @@ function menu(){
             menu()
             break;
         case '2': 
+            let tele
+            let telefones=[]
             let nome = prompt('Digite um nome: ')
             let email = prompt('Digite um e-mail: ')
-            let numero = prompt('Digite um Número: ')
-            adicionar({nome: nome, email: email,numero: numero})
-            console.log('Adicionado com Sucesso')
+            for(let y=0; y < contatos.length; y++){
+                if(contatos[y].email == email){
+                    throw new Error('e-mail já cadastrado')
+                }
+            }
+            while((tele = prompt('Digite seu número de telefone(Deixe em branco se quiser sair): '))){
+                telefones.push(tele)
+            }
+            adicionar({nome: nome, email: email, numero: telefones})
+            console.log('Cadastrado com Sucesso')
             menu()
             break;
         case '3':
@@ -36,11 +45,57 @@ function menu(){
                 if(id == -1){
                     console.log('id não encontrado')
                 }else{
-                    let novoNome= prompt('Digite um novo nome: ')
-                    let novoEmail= prompt('Digite um novo e-mail: ')
-                    let novoNumero= prompt('Digite um novo Número: ')
-                    editar(id, {id: contatos[id].id, nome: novoNome, email: novoEmail, numero: novoNumero})
-                    console.log('Adicionado com Sucesso')
+                    let re = prompt('Tem certeza que deseja editar esse contato? (Sim/Nao): ').toLowerCase()
+                    switch(re){
+                        case 'sim':
+                            let perg =prompt('Digite o que você quer editar (nome/email/numero/todos): ').toLowerCase()
+                            switch(perg){
+                                case 'nome':
+                                    let novoNome= prompt('Digite um novo nome: ')
+                                    contatos[id].nome = (novoNome)
+                                    console.log('Editado com Sucesso')
+                                    menu()
+                                    break;
+                                case 'email':
+                                    let novoEmail= prompt('Digite um novo e-mail: ')
+                                    contatos[id].email = (novoEmail)
+                                    console.log('Editado com Sucesso')
+                                    menu()
+                                    break;
+                                case 'numero':
+                                    let novoNumero
+                                    let numeros = []
+                                    while(novoNumero= prompt('Digite um novo Número (Deixe em branco se quiser sair): ')){
+                                        numeros.push(novoNumero)
+                                    }
+                                    contatos[id].numero = (numeros)
+                                     console.log('Editado com Sucesso')
+                                     menu()
+                                    break;
+                                case 'todos':
+                                    let novonNumero
+                                    let num = []
+                                    let novonNome= prompt('Digite um novo nome: ')
+                                    let novonEmail= prompt('Digite um novo e-mail: ')
+                                    while(novonNumero= prompt('Digite um novo Número (Deixe em branco se quiser sair): ')){
+                                        num.push(novonNumero)
+                                    }
+                                     editar(id, {id: contatos[id].id, nome: novonNome, email: novonEmail, numero: num})
+                                    console.log('Editado com Sucesso')
+                                    menu()
+                                    break;
+                                default:
+                                    throw new Error('Opção Invalida')
+                            }
+                            menu()
+                            break;
+                        case 'nao':
+                            console.log('Retornando ao menu')
+                            break;
+                        default:
+                            throw new Error('Opção Invalida')
+
+                    }
                 }
                 menu()
               }
@@ -55,8 +110,19 @@ function menu(){
                 if(id == -1){
                     console.log('id não encontrado')
                 }else{
-                    excluir(id)
-                    console.log('Excluido com Sucesso')
+                    let exc = prompt('Deseja realmente excluir? (Sim/Nao): ').toLowerCase()
+                    switch(exc){
+                        case 'sim':
+                            excluir(id)
+                            console.log('Excluido com Sucesso')
+                            break;
+                        case 'não':
+                            console.log('Retornando ao menu')
+                            break;
+                        default:
+                            throw new Error('Opção Invalida')     
+                    }
+                    
                 }
                 menu()
             }
--- listar.js
+++ listar.js
@@ -2,7 +2,14 @@ let contatos = require('./contatos')
 
 function listar(){
     contatos.forEach((contato)=>{
-        console.log(`id: ${contato.id} nome: ${contato.nome} email: ${contato.email} numero: ${contato.numero}`)
+        console.log(`id: ${contato.id} 
+nome: ${contato.nome} 
+email: ${contato.email}
+numeros: `)
+for(let f=0; f < contato.numero.length; f++){
+    console.log(`${f+1}: ${contato.numero[f]}`)
+}
+console.log('')
     })
 }
 
